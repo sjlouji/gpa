c395ac35099f518c0da22ec62beb34a68bb10807
selvaprasath2706 <48859019+selvaprasath2706@users.noreply.github.com>
2019-08-23 21:03:09 +0530

    Initial commit

---
+++ selvahome.php
@@ -0,0 +1,10 @@
+<h1>Welcome to GPA Calculator</h1>
+<a href="selva1.php" />Semester 1</a><br>
+<a href="selva2.php"/>Semester 2</a><br>
+<a href="selva3.php"/>Semester 3</a><br>
+<a href="selva4.php"/>Semester 4</a><br>
+<a href="selva5.php"/>Semester 5</a><br>
+<a href="selva6.php"/>Semester 6</a><br>
+<a href="selva7.php"/>Semester 7</a><br>
+<a href="selva8.php"/>Semester 8</a><br>
+<a href="selvahome.php"/>HOME</a><br>
